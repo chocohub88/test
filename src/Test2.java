689e03ee0da7adae62144e9091ce17da784094df
choco <chocohub88@gmail.com>
2026-06-09 21:06:16 -0700

    test 2 update

--- src/Test2.java
+++ src/Test2.java
@@ -1,7 +1,6 @@
 public class Test2 {
     static void main() {
-        System.out.println("github");
         System.out.println("java in action");
-        System.out.println("this is test 2");
+        System.out.println("this is second test");
     }
 }
